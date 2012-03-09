48ffedb9ba4291e0dd78c4d3d2e379fd3c96e2cc
Matthias Luedtke <matthias.luedtke@dynport.de>
2012-03-09 17:19:58 +0100

    Update basic.rb

--- basic.r
+++ basic.r
@@ -1,10 +1,11 @@
 
-# R --vanilla < basic.r
+# R -q --vanilla < basic.r
 
 dtaframe <- read.table("data/nums", sep = ";")
 
 colummn <- dtaframe$V2
+# dput(column)
 
 print(range(colummn))
 print(summary(colummn))
-print(quantile(colummn, c(.75, .90, .98, .99, .999, .9999)))
+print(quantile(colummn, c(.50, .75, .90, .98, .99, .999, .9999, .99999, 1.0)))
