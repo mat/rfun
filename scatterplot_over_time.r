0a8d9d66787e40d3b911f8a6c0a569b44c99ba55
Matthias Luedtke <matthias.luedtke@dynport.de>
2012-03-18 14:14:12 +0100

    Use headers in scatterplot_over_time.r

--- scatterplot_over_time.r
+++ scatterplot_over_time.r
@@ -1,20 +1,27 @@
 library(ggplot2)
 
-dtaframe <- read.table("data/timestamp_count.csv", sep = ",")
 
-dtaframe$V1 <- as.POSIXct(dtaframe$V1, tz="GMT", format="%Y-%m-%dT%H:%M:%S")
+# 2012-03-16T10:56:42+01:00,91
+# 2012-03-16T11:37:36+01:00,75
+# ...
+# 2012-03-17T12:18:34+01:00,49
+dtaframe <- read.table("data/timestamp_count.csv", sep = ",", header=TRUE)
+dtaframe$time <- as.POSIXct(dtaframe$time, tz="GMT", format="%Y-%m-%dT%H:%M:%S")
 
-print(range(dtaframe$V1))
-print(summary(dtaframe$V1))
+print(range(dtaframe$time))
+print(summary(dtaframe$time))
 
-print(range(dtaframe$V2))
-print(summary(dtaframe$V2))
+print(range(dtaframe$count))
+print(summary(dtaframe$count))
 
 # p <- ggplot(dtaframe, aes(x=V1,y=V2))
 # p + geom_point()
 
-p <- ggplot(dtaframe, aes(x=V1,y=V2))
-p + geom_point() + scale_x_datetime(name="Time")
+# p <- ggplot(dtaframe, aes(x=time,y=count))
+# p + geom_point() + scale_x_datetime()
 
 # p <- ggplot(dtaframe, aes(x=V1,y=V2))
 # p + geom_point() + scale_x_datetime(name="Time", major = "6 hours")
+
+p <- ggplot(dtaframe, aes(x=time,y=count))
+p + geom_point() + scale_x_datetime() + opts(axis.text.x=theme_text(angle=90))
