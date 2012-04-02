504f1ad0cd5f450c30d03da59430351af9c07299
Matthias Luedtke <matthias.luedtke@dynport.de>
2012-04-02 17:50:55 +0200

    Update basic.r

--- basic.r
+++ basic.r
@@ -1,11 +1,14 @@
 
 # R -q --vanilla < basic.r
 
-dtaframe <- read.table("data/nums", sep = ";")
+dtaframe <- read.table("data/nums", sep = ";", header=TRUE)
 
-colummn <- dtaframe$V2
+dtaframe <- subset(dtaframe, count >= 100)
+
+colummn <- dtaframe$count
 # dput(column)
 
 print(range(colummn))
 print(summary(colummn))
 print(quantile(colummn, c(.50, .75, .90, .98, .99, .999, .9999, .99999, 1.0)))
+
